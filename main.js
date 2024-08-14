1cd5eb1414254cf3e2ae558365eceb52b8828297
Alisha Kiran <alishasatheesan1992@gmail.com>
2024-08-14 22:54:35 +0400

    modified

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ console.log(`number : ${num}`);
 
 if(num > 1000000){
     alert(`You have entered an invalid number , Number should be less than or equal to 1000000`);
+}if(num <= 0){
+    alert(`You have entered entered 0 or less than 0`)
 } else {
     // console.log(`type : ${typeof(num)}`);
     document.write(`Sum of entered number is : ${addNum(num)}`);
@@ -11,8 +13,9 @@ if(num > 1000000){
 function addNum(number){
     let sum = 0;
     console.log(number.length)
-    for(let i=0; i<number.length; i++){
-        sum = sum + parseInt(number[i]);
+    number = parseInt(number)
+    for(let i=1; i<=number; i++){
+        sum = sum + i;
 
     }
     return sum;
